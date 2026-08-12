bf4dcd90602e1a90ebadcb74b2ab6b6c77bbd5ad
Administrator <Administrator@Anjali>
2026-08-12 11:31:00 +0530

    4th commit

---
+++ programs/src/Ex1.java
@@ -0,0 +1,11 @@
+import pack.Math1;
+import pack1.Math;
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Math1 m1=new Math1(10,20);
+		m1.add();
+		m1.mul();
+	}
+
+}
---
+++ programs/src/pack/Math1.java
@@ -0,0 +1,14 @@
+package pack;
+public class Math1 {
+	public int a,b;
+	public Math1(int a, int b){
+		this.a=a;
+		this.b=b;
+		}
+public void add(){
+System.out.println("Addition: "+(a+b));
+}
+public void mul(){
+System.out.println("Product: "+(a*b));
+}
+}
